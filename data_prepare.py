196fd0aae804a71376edee99c2252cf481d00025
s9265314 <s9265314@gmail.com>
2021-04-25 16:57:31 +0800

    data_pre df

--- data_prepare.py
+++ data_prepare.py
@@ -11,19 +11,21 @@ import re
 from opencc import OpenCC
 from sklearn.utils import shuffle
 #%%
-df = pd.read_excel('data0.xlsx')
-df1 = pd.read_csv('news1.csv')
+
 
 #%%
-b=0
-for i in range(30000,45000):
-    a=len(str(df1.iloc[i,0]))
-    if a>10 and a<50:
-        b+=1
-        new=pd.DataFrame({'txt':df1.iloc[i,0]},index=[1])
-        df=df.append(new,ignore_index=True)
-    if b >=1850:
-        break
+#合併d
+def mergedata(df,df1):
+    b=0
+    for i in range(30000,45000):
+        a=len(str(df1.iloc[i,0]))
+        if a>10 and a<50:
+            b+=1
+            new=pd.DataFrame({'txt':df1.iloc[i,0]},index=[1])
+            df=df.append(new,ignore_index=True)
+        if b >=1850:
+            break
+    return df
 #%%
 dfa = pd.read_excel('data0.xlsx')
 dfb = pd.read_excel('data1.xlsx')
@@ -85,33 +87,30 @@ def s2tw(df):
         df.iloc[i,0]=cc.convert(df.iloc[i,0])
     return df
 #%%
-#%%
-for i in range(0,len(df)):
-    try:
-        df.iloc[i,0]=find_chinese(df.iloc[i,0])
-    except:
-        df.iloc[i,0]=None
-df=df.dropna(axis=0, how='any')
-
-
-
-#%%
 #刪除指定cell
 #df=df.drop([150],axis = 0,inplace = False)
 #%%
-df = del_HoL_seq(df)
-df = del_same(df)
-df = s2tw(df)
-df = shuffle(df)
-df1 = del_HoL_seq(df1)
-df1 = del_same(df1)
-df1 = s2tw(df1)
-df1 = shuffle(df1)
-for i in range(11300,len(df)):
-    df.iloc[i,1]=str(1)
-for i in range(11300,len(df1)):
-    df1.iloc[i,1]=str(0)
+if __name__ == '__main__':
+    df = pd.read_excel('ad0.xlsx')
+    df1 = pd.read_csv('news1.csv')
 
+    df=df.dropna(axis=0, how='any')
+    df1=df1.dropna(axis=0, how='any')
 
-df.to_excel('data_0-22tw1_0105.xlsx',index=False,header=False)
-df1.to_excel('news_0-22tw1_0105.xlsx',index=False,header=False)
+    df = del_HoL_seq(df)
+    df = del_same(df)
+    df = s2tw(df)
+    
+    df1 = del_HoL_seq(df1)
+    df1 = del_same(df1)
+    df1 = s2tw(df1)
+    for i in range(11300,len(df)):
+        df.iloc[i,1]=str(1)
+    for i in range(11300,len(df1)):
+        df1.iloc[i,1]=str(0)
+
+    df.to_excel('ad_0-22tw1_0105.xlsx',index=False,header=False)
+    df1.to_excel('news_0-22tw1_0105.xlsx',index=False,header=False)
+    df = mergedata(df,df1)
+    df = shuffle(df)
+    df.to_excel('data_0-22tw1_0105.xlsx',index=False,header=False)
